a19f827e2dabd9f13c2366c22bfb404da1f1ff80
probitie <viacheslav.mashkov@gmail.com>
2023-01-19 19:20:21 +0200

    draft solution for parenthesses task

--- main.cpp
+++ main.cpp
@@ -2,73 +2,56 @@
 #include <vector>
 #include <iterator>
 #include <algorithm>
+#include <stack>
 
 using namespace std;
 
 /*
- *
- *
- *
- *
- *TODO rewrite
- *
- *
- get min length
- take first word
- peek one letter
- make sure it is common in all words
- if true > add to result prefix, go check next char
- if false > return current result prefix
-
- if all letters chekcked(min length)
- return prefix
- --|
-  tele
-  telephone
-  tell
-  temeria
-
 
 */
 
 #include <unordered_map>
 #include <sstream>
 
-string solve(const std::vector<std::string>  &strs)
-{    
-    // get min length
-
-    int min_len = std::min_element(strs.begin(), strs.end(), [](const string &left, const string &right) -> bool{
-        return left.length() < right.length();
-    })->length();
-
-    std::string res;
-    char pref_char;
-
-    if (min_len == 0) return "";
-
-    for (int i = 0; i < min_len; i++)
+bool is_pair(char left_parenthes, char right_parenthes)
+{
+    std::unordered_map<char, char> pairs
     {
-        pref_char = strs.at(0).at(i);
-        for (auto &str : strs)
-        {
-            if (str.at(i) != pref_char)
-            {
-                goto end;
-            }
-        }
-        res += pref_char;
+        {'(', ')'},
+        {'{', '}'},
+        {'[', ']'}
+    };
+
+    // if map has this key and map[key].second == right_parenthes   ===> return true else return false
+    return false;
+}
+
+bool solve(const std::string  &str)
+{    
+    std::stack<char> stack;
+
+    for(char ch: str)
+    {
+        if (is_pair(stack.top(), ch))
+            stack.pop();
+        else
+            stack.push(ch);
     }
-    end:
-    return res;
+    return stack.size() == 0;
 }
 
 int main()
 {
-    std::cout << solve({"test", "testtest", "test_s", "te"}) << std::endl;
-    std::cout << solve({"aaa", "aaa", ""}) << std::endl;
-    std::cout << solve({""}) << std::endl;
-    std::cout << solve({"a"}) << std::endl;
+    std::cout << std::boolalpha;
+    std::cout << (solve("()[]{}") == true) << std::endl;
+    std::cout << (solve("()]{}") == false) << std::endl;
+    std::cout << (solve("()[]}") == false) << std::endl;
+    std::cout << (solve("()") == true) << std::endl;
+    std::cout << (solve("(") == false) << std::endl;
+    std::cout << (solve("") == true) << std::endl;
+
+
+
 
     return 0;
 }
