493e3bd9d1eb300b686ae742e8b9f93b2d348e1b
probitie <viacheslav.mashkov@gmail.com>
2023-01-25 23:23:44 +0200

    remove element 100% speed

--- main.cpp
+++ main.cpp
@@ -9,28 +9,32 @@ using namespace std;
 
 
 int removeElement(vector<int>& nums, const int val) {
+    if (nums.empty()) return 0;
     int *startpos = &nums[0]; // nums.data()
     int *v = startpos;
     int *x = startpos;
     size_t size = nums.size();
     int *endpos = startpos + size;
+    int res_size = 0;
 
     while (x < endpos)
     {
-        if (*x != val) ++v;
+        if (*x != val) {++v; ++res_size;}
         ++x;
+        if (x >= endpos) break;
         *v = *x;
-
     }
-
-    return std::distance(startpos, v);
+    return res_size;
 }
 
 
 int main()
 {
-
-    std::vector<int> nms = {1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 5, 5, 5, 5};
+    //std::vector<int> nms = {};
+    //std::vector<int> nms = {3};
+    //std::vector<int> nms = {3, 3};
+    //std::vector<int> nms = {1, 1};
+    std::vector<int> nms = {3, 2, 2, 3};
     int res_len = removeElement(nms, 2);
-    return 0;
+    return res_len;
 }
