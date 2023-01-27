fac7d4f3def5582b8f591444280dfa5adfe63b5f
probitie <viacheslav.mashkov@gmail.com>
2023-01-27 11:02:02 +0200

    search insert index - works for existing values

--- main.cpp
+++ main.cpp
@@ -5,28 +5,44 @@
 #include <stack>
 #include <unordered_map>
 #include <sstream>
-using namespace std;
+using std::vector;
 
+int searchInsert(vector<int>& nums, int target) {
+    int * vecdata = nums.data();
+    int min_index = 0;
+    int max_index = nums.size();
+    int distance = max_index - min_index;
 
-int removeElement(vector<int>& nums, const int val) {
-    if (nums.empty()) return 0;
-    int *startpos = &nums[0]; // nums.data()
-    int *v = startpos;
-    int *x = startpos;
-    size_t size = nums.size();
-    int *endpos = startpos + size;
-    int res_size = 0;
+#define mid vecdata[middle_index]
+#define min vecdata[min_index]
+#define max vecdata[max_index]
 
-    while (x < endpos)
+    int middle_index = (min_index + max_index)/2;
+    while(vecdata[middle_index] != target)
     {
-        if (*x != val) {++v; ++res_size;}
-        ++x;
-        if (x >= endpos) break;
-        *v = *x;
-    }
-    return res_size;
-}
+        if (target > mid)
+        {
+            min_index = middle_index;
+        }
+        else
+        {
+            max_index = middle_index;
+        }
+        distance = max_index - min_index;
 
+        if (distance <= 1 && target == min)
+        {
+            return min_index;
+        }
+        else if (distance <= 1)
+        {
+            return max_index;
+        }
+
+        middle_index = min_index + (distance/2);
+    }
+    return middle_index;
+}
 
 int main()
 {
@@ -34,7 +50,8 @@ int main()
     //std::vector<int> nms = {3};
     //std::vector<int> nms = {3, 3};
     //std::vector<int> nms = {1, 1};
-    std::vector<int> nms = {3, 2, 2, 3};
-    int res_len = removeElement(nms, 2);
-    return res_len;
+    std::vector<int> nms = {1, 3, 5, 6};
+    int res_index = searchInsert(nms, 3);
+    std::cout << res_index << "\n";
+    return res_index;
 }
